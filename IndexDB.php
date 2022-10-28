666095787b5d8e2f063627a396d799f2645ae7f7
dayanachiquis <dayanachiquis@gmail.com>
2022-10-27 19:49:37 -0500

    Login pagina conexion a BD 27/10/2022

---
+++ IndexDB.php
@@ -0,0 +1,40 @@
+<?php
+$usuario = "root";
+$nombre_base_de_datos = "factura";
+$inUsuario = $_POST['inUsuario'];
+$inPassword = $_POST['inPassword'];
+
+/*
+isset  pregunta si la variable tiene un valor
+!empty pregunta si la variable no esta vacia
+si inUsuario esta con algun valor? Y la variable in insuario esta doferente de vacia? 
+*/
+
+if(isset( $_POST['inUsuario']) && !empty( $_POST['inUsuario']) &&
+    isset( $_POST['inPassword']) && !empty( $_POST['inPassword']))
+    {
+        try{
+            $base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario);
+        }catch(Exception $e){
+            echo "Ocurrió algo con la base de datos: " . $e->getMessage();
+        }
+
+        $stmt = $base_de_datos->prepare("SELECT COUNT(*) FROM cliente WHERE inNombre = ? AND inContacto = ?");
+        $stmt->execute([$inUsuario, $inPassword]);
+        $count = $stmt->fetchColumn();
+
+        if($count > 0)
+        {
+            header("Location: Administracion.html");            
+        }
+        else{
+            function_alert("We welcome the New World");
+            echo '<script>alert("Usuario o clave invalido!!! intente Nuevamente")</script>';
+            header("Location: login.html");            
+        }
+    }  
+
+    function function_alert($msg) {
+        echo "<script type='text/javascript'>alert('$msg');</script>";
+    }
+?>
--- databaseconnect.php
+++ databaseconnect.php
@@ -5,6 +5,7 @@ $inNombre = $_POST['inNombre'];
 $inEmail = $_POST['inEmail'];
 $inContacto = $_POST['inContacto'];
 $inMensaje = $_POST['inMensaje'];
+$slSolicitud = $_REQUEST['slSolicitud'];
 
 if(isset( $_POST['inNombre']) && !empty( $_POST['inNombre']) &&
     isset( $_POST['inEmail']) && !empty( $_POST['inEmail']) &&
@@ -17,17 +18,18 @@ if(isset( $_POST['inNombre']) && !empty( $_POST['inNombre']) &&
             echo "Ocurrió algo con la base de datos: " . $e->getMessage();
         }
         /*
-            Al incluir el archivo "base_de_datos.php", todas sus variables están
+            Al incluir el archivo "databaseconnect.php", todas sus variables están
             a nuestra disposición. Por lo que podemos acceder a ellas tal como si hubiéramos
             copiado y pegado el código
         */
-        $sentencia = $base_de_datos->prepare("INSERT INTO cliente(inNombre, inEmail, inContacto, inMensaje) VALUES (?,?,?,?);");
-        $resultado = $sentencia->execute([$inNombre, $inEmail, $inContacto, $inMensaje]); # Pasar en el mismo orden de los ?
-        #execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
+        $sentencia = $base_de_datos->prepare("INSERT INTO cliente(inNombre, inEmail, inContacto, inMensaje, slSolicitud) VALUES (?,?,?,?,?);");
+        $resultado = $sentencia->execute([$inNombre, $inEmail, $inContacto, $inMensaje, $slSolicitud]); # Pasar en el mismo orden de los ?
+        #execute regresa un booleano. (1)True en caso de que todo vaya bien, (0)falso en caso contrario.
         #Con eso podemos evaluar
 
         if($resultado === TRUE)
         {
+            echo "Su informacion fue enviada correctamente! Gracias por visitarnos..";
             header("Location: index.html");            
         }
         else{
