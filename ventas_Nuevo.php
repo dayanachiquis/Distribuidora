2e1f89e54c38288ed8ce9d77793faf35b3e904aa
henrypenuela <hepeco1009@hotmail.com>
2022-12-13 22:57:02 -0500

    alerta disponibles  en stock

--- ventas_Nuevo.php
+++ ventas_Nuevo.php
@@ -22,22 +22,36 @@ if( isset( $_POST['inIdFactura']) && !empty( $_POST['inIdFactura']) &&
             a nuestra disposición. Por lo que podemos acceder a ellas tal como si hubiéramos
             copiado y pegado el código
         */
-        $sentencia = $base_de_datos->prepare("INSERT INTO ventas(VenIdFactura, VenCodigo, VenProducto, VenCantidad, VenFecha) VALUES (?,?,?,?, now());");
-        $resultado = $sentencia->execute([$inIdFactura, $inCodigo, $inProducto, $inCantidad]); # Pasar en el mismo orden de los ?
 
-        $sentencia = $base_de_datos->prepare("UPDATE producto SET ProExistencia = ProExistencia - ? WHERE ProCodigo = ?;");
-        $resultado = $sentencia->execute([$inCantidad, $inCodigo]); # Pasar en el mismo orden de los ?
+        $stmt = $base_de_datos->prepare("SELECT CASE WHEN ? <= ProExistencia THEN '1' ELSE '0' END FROM producto WHERE ProCodigo = ?;");
+        $stmt->execute([$inCantidad, $inCodigo]);
+        $resultValidacion = $stmt->fetchColumn();
 
-        #execute regresa un booleano. (1)True en caso de que todo vaya bien, (0)falso en caso contrario.
-        #Con eso podemos evaluar
-
-        if($resultado === TRUE)
+        if($resultValidacion == 1)
         {
-            echo "Su informacion fue enviada correctamente! Gracias por visitarnos..";
-            header("Location: ventas.php");            
+            $sentencia = $base_de_datos->prepare("INSERT INTO ventas(VenIdFactura, VenCodigo, VenProducto, VenCantidad, VenFecha) VALUES (?,?,?,?, now());");
+            $resultado = $sentencia->execute([$inIdFactura, $inCodigo, $inProducto, $inCantidad]); # Pasar en el mismo orden de los ?
+    
+            $sentencia = $base_de_datos->prepare("UPDATE producto SET ProExistencia = ProExistencia - ? WHERE ProCodigo = ?;");
+            $resultado = $sentencia->execute([$inCantidad, $inCodigo]); # Pasar en el mismo orden de los ?    
+
+             #execute regresa un booleano. (1)True en caso de que todo vaya bien, (0)falso en caso contrario.
+            #Con eso podemos evaluar
+
+            if($resultado === TRUE)
+            {
+                echo "Su informacion fue enviada correctamente! Gracias por visitarnos..";
+                header("Location: ventas.php");            
+            }
+            else{
+                echo "Algo salió mal. Por favor verifica que la tabla exista";
+            }
+
         }
-        else{
-            echo "Algo salió mal. Por favor verifica que la tabla exista";
-        }
+        else
+        {
+            echo '<script>alert("La cantidad solicitada es mayor que la cantidad Disponible en stock"); window.location.href="ventas.php";</script>';  
+        }   
+       
     }
 ?>
